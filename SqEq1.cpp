9fba51fa1945cef8e441c4958f0aa77a5b208d0a
DimaKrasav4eg <70336507+DimaKrasav4eg@users.noreply.github.com>
2020-08-28 12:43:58 +0500

    Add files via upload

---
+++ SqEq1.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<math.h>
+
+int reshy_kvadrat(double a, double b, double c, double* x1, double* x2){
+
+    if ((a == 0) and (b == 0)){
+
+        if (c == 0){
+
+            return NULL;
+        }
+        else{
+
+            *x1 = NULL;
+            *x2 = NULL;
+            return 0;
+        }
+    }
+    else if (a == 0){
+
+        *x1 = -c/b;
+        *x2 = -c/b;
+        return 1;
+    }
+    else{
+
+        double d = 0;
+        d = b*b-4*a*c;
+
+        if (d<0){
+
+            *x1 = NULL;
+            *x2 = NULL;
+            return 0;
+        }
+        else if (d == 0){
+
+            *x1 = -b/(2*a);
+            *x2 = *x1;
+            return 1;
+        }
+        else if (d > 0){
+
+            *x1 = (-b + sqrt(d))/(2*a);
+            *x2 = (-b - sqrt(d))/(2*a);
+            return 2;
+        }
+    }
+}
+
+int main(){
+
+    double a = 0, b = 0, c = 0;
+    scanf("%lg, %lg, %lg", a, b, c);
+
+    double x1 = 0, x2 = 0;
+
+    reshy_kvadrat(a, b, c, &x1, &x2);
+
+    printf("x1 = %lg, x2 = %lg", x1, x2);
+
+}
