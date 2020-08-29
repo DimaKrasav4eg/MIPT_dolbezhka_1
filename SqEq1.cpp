dbc99b3b1237b21d869e56bd3e3b79b9d0ff6c04
DimaKrasav4eg <70336507+DimaKrasav4eg@users.noreply.github.com>
2020-08-29 13:32:05 +0500

    Add files via upload

--- SqEq1.cpp
+++ SqEq1.cpp
@@ -1,6 +1,44 @@
 #include<stdio.h>
 #include<math.h>
 
+//-----------------------------------------------------------------------------
+
+int reshy_kvadrat(double a, double b, double c, double* x1, double* x2);
+
+//-----------------------------------------------------------------------------
+
+int main(){
+
+    printf("Napishi koefficenti kvadratnogo yravnenija:\n");
+
+    double a = 0, b = 0, c = 0;
+    scanf("%lg %lg %lg", &a, &b, &c);
+
+    double x1 = 0, x2 = 0, k = 0;
+
+    k = reshy_kvadrat(a, b, c, &x1, &x2);
+
+    if (k == NULL){
+
+        printf("Beskone4noe kol-vo rjney");
+    }
+    else if (k == 2){
+
+        printf("x1 = %lg, x2 = %lg", x1, x2);
+    }
+    else if (k == 1){
+
+        printf("x = %lg", x1);
+    }
+    else if (k == 0){
+
+        printf("Kornej net");
+    }
+
+}
+
+//-----------------------------------------------------------------------------
+
 int reshy_kvadrat(double a, double b, double c, double* x1, double* x2){
 
     if ((a == 0) and (b == 0)){
@@ -11,15 +49,15 @@ int reshy_kvadrat(double a, double b, double c, double* x1, double* x2){
         }
         else{
 
-           // *x1 = NULL;
-           // *x2 = NULL;
+            *x1 = NULL;
+            *x2 = NULL;
             return 0;
         }
     }
     else if (a == 0){
 
         *x1 = -c/b;
-        *x2 = -c/b;
+        *x2 = NULL;
         return 1;
     }
     else{
@@ -28,14 +66,14 @@ int reshy_kvadrat(double a, double b, double c, double* x1, double* x2){
 
         if (d<0){
 
-            //*x1 = NULL;
-            //*x2 = NULL;
+            *x1 = NULL;
+            *x2 = NULL;
             return 0;
         }
         else if (d == 0){
 
             *x1 = -b/(2*a);
-            *x2 = *x1;
+            *x2 = NULL;
             return 1;
         }
         else if (d > 0){
@@ -47,15 +85,3 @@ int reshy_kvadrat(double a, double b, double c, double* x1, double* x2){
     }
 }
 
-int main(){
-
-    double a = 0, b = 0, c = 0;
-    scanf("%lg, %lg, %lg", &a, &b, &c);
-
-    double x1 = 0, x2 = 0;
-
-    reshy_kvadrat(a, b, c, &x1, &x2);
-
-    printf("x1 = %lg, x2 = %lg", x1, x2);
-
-}
