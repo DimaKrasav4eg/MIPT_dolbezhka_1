c05e036550d18c0a59304c6d00498e7665255b14
DimaKrasav4eg <70336507+DimaKrasav4eg@users.noreply.github.com>
2020-09-15 13:22:09 +0300

    Add files via upload

---
+++ SqEq3.cpp
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include<math.h>
+
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//! Func reshy_kvadrat() solves the square equation ax^2 + bx + c = 0
+//!
+//! @param [in]  a  | a  - coefficient
+//! @param [in]  b  | b  - coefficient
+//! @param [in]  c  | c  - coefficient
+//! @param [out] x1 | x1 - 1 root
+//! @param [out] x2 | x2 - 2 root
+//!
+//! @return number of roots
+//!
+//! @note If the number of roots is infinite -1
+//-----------------------------------------------------------------------------
+
+int reshy_kvadrat(double a, double b, double c, double* x1, double* x2);
+
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//! Func is_zero() compares a number to zero
+//!
+//! @param [in]  a  | a  - number
+//!
+//! @return True or False
+//-----------------------------------------------------------------------------
+
+int is_zero(double num);
+
+//-----------------------------------------------------------------------------
+
+const float Zero = 1e-5;
+
+//-----------------------------------------------------------------------------
+
+int main(){
+
+    printf("write the coefficients of the square equation:\n");
+
+    double a = 0,
+           b = 0,
+           c = 0;
+    scanf("%lg %lg %lg", &a, &b, &c);
+
+    double x1            = 0,
+           x2            = 0;
+    int    kol_vo_korney = reshy_kvadrat(a, b, c, &x1, &x2);
+
+    switch (kol_vo_korney){
+
+        case  0:
+            printf("no roots");
+            break;
+        case  1:
+            printf("x = %lg", x1);
+            break;
+        case  2:
+            printf("x1 = %lg, x2 = %lg", x1, x2);
+            break;
+        case -1:
+            printf("infinite number of roots");
+            break;
+    }
+
+}
+
+//-----------------------------------------------------------------------------
+
+int reshy_kvadrat(double a, double b, double c, double* x1, double* x2){
+
+    if ((is_zero(a)) and (is_zero(b))){
+
+        if (is_zero(c)){
+
+            return -1;
+        }
+        else {
+
+            return 0;
+        }
+    }
+    else if (is_zero(a)){
+
+        *x1 = -c / b;
+        return 1;
+    }
+    else {
+
+        double d = b * b - 4 * a * c;
+
+        if (d < 0){
+
+            return 0;
+        }
+        else if (is_zero(d)){
+
+            *x1 = - b / (2 * a);
+            return 1;
+        }
+        else if (d > 0){
+
+            *x1 = (- b + sqrt(d))/(2 * a);
+            *x2 = (- b - sqrt(d))/(2 * a);
+            return 2;
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+int is_zero(double num){
+
+    return (fabs(num) < Zero);
+}
+
+//-----------------------------------------------------------------------------
+
